b9d31d287f917f3931232354fa6b896e4ee8270a
greccy <greccy.verde@gmail.com>
2026-01-15 21:46:48 +0100

    Fix: validando moneda

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -7,6 +7,7 @@ import java.net.http.HttpResponse;
 
 public class ConsultaMoneda {
 
+    private final Gson gson = new Gson();
     public Moneda conversionMoneda(String baseCode, String targetCode , double amountConversion,HttpClient client){
         URI direccion = URI.create("https://v6.exchangerate-api.com/v6/731e3363a576125e05b90c77/pair/" + baseCode + "/" + targetCode + "/" + amountConversion);
 
@@ -19,7 +20,15 @@ public class ConsultaMoneda {
             if (response.statusCode() != 200) {
                 throw new RuntimeException("La API respondió con error HTTP: " + response.statusCode());
             }
-            return new Gson().fromJson(response.body(), Moneda.class);
+
+            Moneda moneda = gson.fromJson(response.body(), Moneda.class);
+            if (moneda == null) {
+                throw new RuntimeException("No se pudo interpretar la respuesta de la API.");
+            }
+            return moneda;
+
+        } catch (RuntimeException e) {
+            throw e;
         } catch (Exception e ) {
             System.out.println(e.getMessage());
             throw new RuntimeException("No se pudo realizar la conversión");
