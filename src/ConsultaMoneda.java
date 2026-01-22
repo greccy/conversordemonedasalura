82bba69b6ec9182c421f687fb24d522fc2f60a09
greccy <greccy.verde@gmail.com>
2026-01-22 21:10:17 +0100

    Add: Variable de entorno para API

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -6,11 +6,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ConsultaMoneda {
-
+    private static final String API_KEY = System.getenv("EXCHANGE_API_KEY");
     private final Gson gson = new Gson();
-    public Moneda conversionMoneda(String baseCode, String targetCode , double amountConversion,HttpClient client){
-        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/731e3363a576125e05b90c77/pair/" + baseCode + "/" + targetCode + "/" + amountConversion);
 
+    public Moneda conversionMoneda(String baseCode, String targetCode , double amountConversion,HttpClient client){
+        if (API_KEY == null || API_KEY.isBlank()) {
+            throw new RuntimeException("API Key no configurada. Define la variable de entorno EXCHANGE_API_KEY");
+        }
+
+        URI direccion = URI.create(
+                "https://v6.exchangerate-api.com/v6/" + API_KEY +
+                        "/pair/" + baseCode + "/" + targetCode + "/" + amountConversion
+        );
 
         HttpRequest request = HttpRequest.newBuilder().uri(direccion).build();
 
